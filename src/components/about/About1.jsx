21bcd5b307de0f9e85d87f25eaeacde958432472
Marc Rivas <76574248+mrevas@users.noreply.github.com>
2023-06-04 11:58:07 -0400

    init

---
+++ src/components/about/About1.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+function About1() {
+  return (
+    <>
+        <div className="about-section pt-120 pb-120" id="about">
+    <img src="assets/images/bg/section-bg1.svg" alt="image" className="section-bg1 img-fluid" />
+    <div className="container">
+        <div className="row">
+        <div className="col-lg-6 text-lg-start text-center wow fadeInDown" data-wow-duration="1.5s" data-wow-delay="200ms">
+            <div className="section-title1">
+            <h2>About Diamond Back Funding</h2>
+            <p>We&apos;re not just any old funding company; we&apos;re more like the diamond-encrusted superstars of financial assistance. 
+                When life throws you a legal curveball, we swoop in to save the day. Think of us as your trusty sidekick, helping you slay those legal dragons with ease.</p>
+            </div>
+            <div className="about-content1">
+            <h3>Get about our law firm and learn about our expertise.</h3>
+            <div className="lawyer-word">
+                <p>In consequat tincidunt turpis sit amet imperdiet. Praesent Classei 
+                consequat tincidunt turpis sit amet imperdiet for mind.</p>
+                <img src="assets/images/icons/lawyer-sign.svg" alt="image" />
+                <h6>Simanto Rahman, CEO-Founder</h6>
+            </div>
+            </div>
+        </div>
+        <div className="col-lg-6 d-flex justify-content-lg-end justify-content-center">
+            <div className="about1-img wow fadeInRight" data-wow-duration="1.5s" data-wow-delay="0.2s">
+            <img src="assets/images/bg/about1-bg.png" alt="image" className="img-fluid" />
+            <div className="experience-tag">
+                <h2>05 <span>Years</span></h2>
+                <p>We Have A Lot Of Real Experience</p>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+   
+    </>
+  )
+}
+
+export default About1
